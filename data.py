fc90e820dbd108c6c1bb809e1be1262c58e5e4dd
kushaldhinoja1869 <45332109+kushaldhinoja1869@users.noreply.github.com>
2022-11-07 16:13:50 +0000

    Add files via upload

---
+++ data.py
@@ -0,0 +1,26 @@
+
+def getchannel():
+
+	chl = {
+		0: "&TV",
+		1: "Animal Planet",
+		2: "Colors",
+		3: "Discovery Channel",
+		4: "History TV",
+		5: "Hungama",
+		6: "Nick",
+		7: "Pogo",
+		8: "Sonic",
+		9: "Sony SAB",
+		10: "Sony PAL",
+		11: "Sony YAY",
+		12: "Star Sports Hindi 1",
+		13: "TLC",
+		14: "UTV Movies",
+		15: "Star Bharat",
+		16: "UTV Action"
+	}
+	return chl
+
+def getmodelname():
+	return "1.tflite"
